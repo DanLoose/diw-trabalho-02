236d53047f9f153d08ad4c7c9b5aef68ddeff628
DanLoose <danloose14@gmail.com>
2023-06-06 19:07:06 -0300

    Criação de partials de navbar e footer

---
+++ js/Partials/footer.js
@@ -0,0 +1,16 @@
+const footer = document.querySelector("#footer");
+footer.innerHTML +=
+    `<!-- Informações Aluno -->
+    <div class="container" id="sobre-mim">
+        <div class="row">
+            <div class="col p-3">
+
+                <small><span>Trabalho desenvolvido por: </span> </small> <br>
+                <h5> Danilo Mascarenhas Loose </h5>
+                <p class="m-0"> Sistemas de Informação Noturno</p>
+                <p class="m-0"> PUC Minas - São Gabriel - 2023/1 </p>
+                <p class="m-0"> Matrícula: 00809494</p>
+
+            </div>
+        </div>
+    </div>`
---
+++ js/Partials/navbar.js
@@ -0,0 +1,38 @@
+const navbar = document.querySelector("#navbar");
+navbar.innerHTML +=
+    `
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+
+            <!-- brand  -->
+            <a class="navbar-brand" href="/index.html">Claudia's Ecommerce</a>
+
+            <!-- botao  -->
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <!-- itens escondidos  -->
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="/index.html">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/screens/sobremim.html">Sobre Mim</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                            aria-expanded="false">
+                            Categorias
+                        </a>
+                        <ul id="categorias-header" class="dropdown-menu">
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+`
